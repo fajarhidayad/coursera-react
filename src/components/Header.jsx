99717f7cbe1268de1e3e0e2cccb5c206e37bf3ec
Fajar Hidayad <fajarsuryahidayad@gmail.com>
2023-09-07 09:34:05 +0700

    separate components and add meta tags

---
+++ src/components/Header.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <header>
+      <meta
+        name="description"
+        content="Little lemon is an online restaurant reservations"
+      />
+      <meta name="og:title" content="Little Lemon" />
+      <meta name="og:description" content="The best restaurant" />
+      <meta name="og:image" content="" />
+    </header>
+  );
+};
+
+export default Header;
